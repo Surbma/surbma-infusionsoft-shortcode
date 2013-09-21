9f54f88e9623568269409f4894cb176fc10f9268
Surbma <peter@surbma.hu>
2013-09-21 17:57:23 +0200

    1.0.0 Infusionsoft Integration

---
+++ surbma-infusionsoft.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Plugin Name: Surbma - Infusionsoft Integration
+Plugin URI: http://surbma.com/
+Description: Integrate Infusionsoft with WordPress
+Version: 1.0
+Author: Surbma
+Author URI: http://surbma.com/
+License: GPL2
+*/
+
+function surbma_add_infusionsoft_form( $atts ) {
+	extract( shortcode_atts( array(
+		"account" => '',
+		"id" => ''
+	), $atts ) );
+	return '<script type="text/javascript" src="https://'.$account.'.infusionsoft.com/app/form/iframe/'.$id.'"></script>';
+}
+add_shortcode( 'infusionsoft-form', 'surbma_add_infusionsoft_form' );
+
